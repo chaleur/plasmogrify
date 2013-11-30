43cd97d418791715750ef3b0d4372c40265ea131
Kaye Mason <chaleur@gmail.com>
2013-11-29 19:37:20 -0800

    Autogenerated gears.

--- plasmogrify/Source/World/Gears/Gear.cpp
+++ plasmogrify/Source/World/Gears/Gear.cpp
@@ -49,7 +49,6 @@ namespace Plasmogrify
 
             void Gear::BuildGear(eGearType gearType)
             {
-#if 1
                 uint32_t numVerts = kSegments + 1;
                 mpModel->CreateVertexList(numVerts);
 
@@ -74,47 +73,6 @@ namespace Plasmogrify
                     }
                 }
 
-#if 0
-                // Add the center vertex.
-                mpModel->SetVertex(vertex, XMFLOAT3(0.0f, center.y, 0.0f), XMFLOAT4(1.0f, 1.0f, 0.0f, 1.0f) );
-                ++vertex;
-				mpModel->SetVertex(vertex, XMFLOAT3(0.5f, 0.0f + center.y, 0.0f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f));
-				++vertex;
-				mpModel->SetVertex(vertex, XMFLOAT3(0.0f, 0.5f + center.y, 0.0f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f));
-				++vertex;
-				mpModel->SetVertex(vertex, XMFLOAT3(-0.5f, 0.0f + center.y, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f));
-				++vertex;
-				mpModel->SetVertex(vertex, XMFLOAT3(0.0f, -0.5f + center.y, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f));
-				++vertex;
-#if 1
-				mpModel->SetIndex(index, 0);
-				++index;
-				mpModel->SetIndex(index, 2);
-				++index;
-				mpModel->SetIndex(index, 1);
-				++index;
-#endif
-				mpModel->SetIndex(index, 0);
-				++index;
-				mpModel->SetIndex(index, 3);
-				++index;
-				mpModel->SetIndex(index, 2);
-				++index;
-
-				mpModel->SetIndex(index, 0);
-				++index;
-				mpModel->SetIndex(index, 4);
-				++index;
-				mpModel->SetIndex(index, 3);
-				++index;
-
-				mpModel->SetIndex(index, 0);
-				++index;
-				mpModel->SetIndex(index, 1);
-				++index;
-				mpModel->SetIndex(index, 4);
-				++index;
-#else
 				static XMFLOAT4 kColorHack[5] =
 				{
 					XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f),
@@ -136,7 +94,7 @@ namespace Plasmogrify
                     float sinAngle = sin(angle) * 0.5f;
 
                     mpModel->SetVertex(vertex, XMFLOAT3(cosAngle, sinAngle + center.y, 0.0f), kColorHack[vertex] );
-#if 1
+
                     mpModel->SetIndex(index, 0);
 					++index;
 
@@ -149,59 +107,9 @@ namespace Plasmogrify
 
                     mpModel->SetIndex(index, vertex);
 					++index;
-#endif
 					
                     ++vertex;
                 }
-
-#if 0
-				mpModel->SetIndex(index, 0);
-				++index;
-				mpModel->SetIndex(index, 2);
-				++index;
-				mpModel->SetIndex(index, 1);
-				++index;
-#endif
-#endif
-
-#else
-
-                uint32_t numVerts = kSegments * 3;
-                mpModel->CreateVertexList(numVerts);
-
-                uint32_t numIndicies = kSegments * 3;
-                mpModel->CreateIndexList(numIndicies);
-
-                XMFLOAT3 center = XMFLOAT3(0.45f, 0.00f, 0.0f);
-                
-                switch ( gearType )
-                {
-                    case kGearType_Small:
-                    {
-                        center = XMFLOAT3(0.00f, 0.10f, 0.0f);
-                        break;
-                    }
-                    case kGearType_Big:
-                    {
-                        center = XMFLOAT3(0.00f, -0.45f, 0.0f);
-                        break;
-                    }
-                }
-				
-                for (uint32_t i = 0; i < kSegments; ++i)
-                {
-                    uint32_t offset = i * 3;
-
-                    mpModel->SetVertex(offset + 0, XMFLOAT3(0.0f, 0.5f + center.y, 0.0f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f));
-                    mpModel->SetVertex(offset + 1, XMFLOAT3(0.45f, 0.0f + center.y, 0.0f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f));
-                    mpModel->SetVertex(offset + 2, XMFLOAT3(-0.45f, 0.0f + center.y, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f));
-
-                    mpModel->SetIndex(offset + 0, offset + 0);
-                    mpModel->SetIndex(offset + 1, offset + 1);
-                    mpModel->SetIndex(offset + 2, offset + 2);
-                }
-#endif
-
             }
 
             void Gear::Update(float dt)
